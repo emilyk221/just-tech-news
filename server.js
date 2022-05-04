f2e5976a812b2b4dfc4e8b094a14dd73f9d55734
emilyk <emily.g.kruse@gmail.com>
2022-05-04 16:32:01 -0400

    add helper functions

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const express = require("express");
 const session = require("express-session");
 const exphbs = require("express-handlebars");
 const routes = require("./controllers");
+const helpers = require("./utils/helpers");
 
 const app = express();
 const PORT = process.env.PORT || 3001;
@@ -23,7 +24,7 @@ const sess = {
 
 app.use(session(sess));
 
-const hbs = exphbs.create({});
+const hbs = exphbs.create({ helpers });
 
 // set up handlebars
 app.engine("handlebars", hbs.engine);
